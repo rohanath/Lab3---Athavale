fe80da9febefffe8ad99148337de0696ee5e3afb
rohanath <rohan.athavale@sjsu.edu>
2017-12-11 13:00:27 -0800

    Junit tests
    Commiting with JUnit tests..

--- SpringBootDemo/tests/com/AbstractTest.java
+++ SpringBootDemo/tests/com/AbstractTest.java
@@ -1,5 +1,4 @@
 package com;
-import com.SpringBootDemo;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
--- SpringBootDemo/tests/com/service/UserServiceTest.java
+++ SpringBootDemo/tests/com/service/UserServiceTest.java
@@ -4,12 +4,15 @@ import com.entity.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
 @Transactional
-public class UserServiceTest extends AbstractTest {
+public class UserServiceTest{
 
     @Autowired
     private UserService userService;
@@ -23,18 +26,40 @@ public class UserServiceTest extends AbstractTest {
         Assert.assertTrue(email.equals(users.get(0).getEmail()));
     }
 
-    /*@Test
-    public void testAddUser() {
-        User user = new User();
-        user.setFirstname("Rohan");
-        user.setLastname("Athavale");
-        user.setEmail("rohan.athavale@sjsu.edu");
-        user.setPassword("rohana");
+    @Test
+    public void testLoginFail() {
 
-        User addedUser = userService.addUser(user);
-        System.out.println(addedUser != null);
-        System.out.println(addedUser.getEmail());
+        String email = "rohanp@gmail.com";
+        String password = "rohana";
+        List<User> users = userService.login(email, password);
+        Assert.assertTrue(users==null);
+    }
 
-        Assert.assertTrue(user.getEmail().equals(addedUser.getEmail()));
-    }*/
+    @Test
+    public void testStar() {
+
+        String username = "mark@gmail.com";
+        String file = "kryptomake.sql";
+        HttpStatus status = userService.starFile(username,file);
+        Assert.assertTrue(status==HttpStatus.OK);
+    }
+
+    @Test
+    public void testDelete() {
+
+        String username = "mark@gmail.com";
+        String file = "kryptomake.sql";
+        HttpStatus status = userService.deleteFile(username,file);
+        Assert.assertTrue(status==HttpStatus.OK);
+    }
+
+    @Test
+    public void testdeleteStar() {
+
+        String username = "mark@gmail.com";
+        String file = "kryptomake.sql";
+        HttpStatus status = userService.deletestarFile(username,file);
+        Assert.assertTrue(status==HttpStatus.OK);
+    }
+
 }
